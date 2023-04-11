669ce61258e5309cd36419c1c5c1dec5c60f0016
karolinakiljanska <karolinakiljanska112@gmail.com>
2023-04-11 21:48:27 +0200

    poprawki2

--- src/js/fetch-images.js
+++ src/js/fetch-images.js
@@ -5,8 +5,12 @@ axios.defaults.baseURL = 'https://pixabay.com/api/';
 const KEY = '35042583-b1dc54e03458557300da2cf10';
 
 async function fetchImages(query, page, perPage) {
-  const response = await axios.get(
-    `?key=${KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`
-  );
-  return response;
+  try {
+    const response = await axios.get(
+      `?key=${KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`
+    );
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
 }
